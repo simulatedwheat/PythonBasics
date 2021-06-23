0405808c898f6c43c53960ed19e97be3c6ebb7e8
Sam <srudqvist@drury.edu>
2021-06-23 19:18:11 +0200

    Added the chaos function

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 #
 # Change Log:
 #           (20210623): Added the Fahrenheit to Celsius converter
+#           (20210623): Added the chaos function
+#
+#
+#
 
 
 
@@ -14,5 +18,16 @@ def FtoC():
     fahrenheit = 9/5 * celsius + 32
     print (celsius, "Celsius converts to: ", fahrenheit, "Fahrenheit")
 
+# A chaotic function
+def chaos():
+    print ("\nThis program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1-x)
+        print(x)
+
+
+
 # Choose what to run
 FtoC()
+chaos()
