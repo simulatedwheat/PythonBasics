56b688327e96b45bc9565bb59b14c593477a74ee
Sam <srudqvist@drury.edu>
2021-06-23 19:28:01 +0200

    Added evaluate()

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 # Fahrenheit to Celsius converter
 def FtoC():
     print ("This program converts temperature from celsius to fahrenheit\n")
+    # eval() is used when the input is a number, it turns the string "32" into the number 32
     celsius = eval(input("Enter the temperature in Celsius: "))
     fahrenheit = 9/5 * celsius + 32
     print (celsius, "Celsius converts to: ", fahrenheit, "Fahrenheit")
@@ -26,8 +27,13 @@ def chaos():
         x = 3.9 * x * (1-x)
         print(x)
 
-
+# Uses eval() to find the answers from mathematical expressions.
+def evaluate():
+    print("\nThis function evaluates the math expression from the user input.")
+    ans = eval(input("Enter an expression: "))
+    print("Answer:", ans)
 
 # Choose what to run
-FtoC()
-chaos()
+#FtoC()
+#chaos()
+evaluate()
