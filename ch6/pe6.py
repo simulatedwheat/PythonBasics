b382f2bb71e37a9e85a3049274e2843d290562b5
Sam <srudqvist@drury.edu>
2021-07-07 23:19:32 +0200

    added stuff

--- ch6/pe6.py
+++ ch6/pe6.py
@@ -33,17 +33,38 @@ def song():
 def sphereArea(radius):
     surfaceArea = 4*math.pi*radius**2
     return print("The surface area is:",surfaceArea)
+
 def sphereVolume(radius):
     volume = 4/3*math.pi*radius**3
     return print("The volume is:",volume)
+    
 def sphere():
     radius = float(input("Enter the radius of a sphere: "))
     sphereArea(radius)
     sphereVolume(radius)
 
+###
+### Number 4
+###
+def sumN(n):
+    sum = 0
+    for i in range(1,n + 1):
+        sum = sum + i
+    return print("The sum of the first",n,"numbers is:",sum)
 
+def sumNCubes(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i**2
+    return print("The cubes of the first",n,"numbers are:",sum)
+
+def numbers():
+    n = int(input("Enter a positive integer: "))
+    sumN(n)
+    sumNCubes(n)
 
     
 # What to run
 #song()
-sphere()
+#sphere()
+numbers()
